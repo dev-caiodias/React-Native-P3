567e796c1c722743fe485196a356593faf5a35b0
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-14 17:05:22 -0300

    feat: estruturação do SocialButton

---
+++ components/SocialButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { styles } from './SocialButton.styles';
+
+type Props = {
+    label: string;
+    icon: any;
+};
+
+export default function SocialButton({ label, icon }: Props) {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Image source={icon} style={styles.icon} />
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
