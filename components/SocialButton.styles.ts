eb3c87f6dfe06ae7699b6072703edc44581a73be
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-14 17:45:05 -0300

    feat: estilização completa do SocialButton

---
+++ components/SocialButton.styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+     button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 50,
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    width: '100%',
+    marginBottom: 12,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 16,
+  },
+  label: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+});
